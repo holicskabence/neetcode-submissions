41894d4027037d1231f039dfb16258f6866af1d6
Bence Holicska <101716648+holicskabence@users.noreply.github.com>
2026-06-29 12:12:22 +0200

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string trimmed = new string(s.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+        int length = trimmed.Length - 1;
+
+        for (int i = 0; i < trimmed.Length; i++){
+            if(trimmed[i] != trimmed[length - i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
