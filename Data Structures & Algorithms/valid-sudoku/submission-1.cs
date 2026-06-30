c5d05b98788f7c220d20f68b5efbe87aa87f9b57
Bence Holicska <101716648+holicskabence@users.noreply.github.com>
2026-06-30 11:05:10 +0200

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        var rows = new HashSet<char>[9];
+        var columns = new HashSet<char>[9];
+        var boxes = new HashSet<char>[9];
+
+        for (int i = 0; i < 9; i++)
+        {
+            rows[i] = new HashSet<char>();
+            columns[i] = new HashSet<char>();
+            boxes[i] = new HashSet<char>();
+        }
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                var current = board[i][j];
+                if(char.IsDigit(current)){
+                    int box = (i / 3) * 3 + (j / 3);
+
+                    if(rows[i].Any(x => x == current) || columns[j].Any(x => x == current) || boxes[box].Any(x => x == current))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        rows[i].Add(current);
+                        columns[j].Add(current);
+                        boxes[box].Add(current);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
