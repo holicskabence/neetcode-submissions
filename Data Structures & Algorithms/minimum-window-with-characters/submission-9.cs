f2669f2b747e9e03f11f75e579e1f572c01a09ff
Bence Holicska <101716648+holicskabence@users.noreply.github.com>
2026-08-05 15:37:04 +0200

    Add: minimum-window-with-characters - submission-9

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-9.cs
@@ -0,0 +1,43 @@
+public class Solution {
+    public string MinWindow(string s, string t) {
+        var need = new Dictionary<char, int>();
+        var window = new Dictionary<char, int>();
+        string result = string.Empty;
+        int left = 0;
+        
+        foreach(var c in t){
+            need[c] = need.GetValueOrDefault(c) + 1;
+        }
+
+        for(int right = 0; right < s.Length; right++){
+            char c = s[right];
+
+            if(need.ContainsKey(c))
+            {
+                if(window.Count == 0){
+                    left = right;
+                }
+
+                if(!window.ContainsKey(c) || need[c] != window[c]){
+                    window[c] = window.GetValueOrDefault(c) + 1;
+                }
+                
+                bool same = need.Count == window.Count && need.All(x => window.ContainsKey(x.Key) && window[x.Key] == x.Value);
+                if(same){
+                    if(string.IsNullOrEmpty(result) || result.Length - 1 >= right - left){
+                        result = s.Substring(left, right - left + 1);
+                    }
+
+                    window.Remove(s[left]);
+                    left++;
+
+                    while(left < right && !window.ContainsKey(s[left])){
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
