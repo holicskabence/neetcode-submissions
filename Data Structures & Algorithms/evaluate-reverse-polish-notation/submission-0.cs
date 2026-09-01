507db7fb02403921459f111d739caf69a098be93
Bence Holicska <101716648+holicskabence@users.noreply.github.com>
2026-09-01 13:43:13 +0200

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        var stack = new Stack<int>();
+        for(int i = 0; i < tokens.Length; i++){
+            string current = tokens[i];
+
+            if(!current.Contains("+") && !current.Contains("-") && !current.Contains("*") && !current.Contains("/"))
+            {
+                stack.Push(int.Parse(current));
+            }
+            else
+            {
+                int result = 0;
+                 while(stack.Count > 0){
+                    int top = stack.Pop();
+
+                    switch(current){
+                        case "+":
+                            result += top;
+                        break;
+                        case "-":
+                            result -= top;
+                        break;
+                        case "*":
+                            result *= top;
+                        break;
+                        case "/":
+                            result /= top;
+                        break;
+                    }
+                }
+
+                stack.Push(result);
+            }
+        }
+
+        return stack.Pop();
+    }
+}
