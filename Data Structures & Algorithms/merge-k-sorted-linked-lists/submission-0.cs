380275fb8beaf3cbf60023b7ce9e6da4d0f60032
Bence Holicska <101716648+holicskabence@users.noreply.github.com>
2026-09-09 12:13:54 +0200

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cs
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {    
+    public ListNode MergeKLists(ListNode[] lists) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        
+        var valids = lists.Where(x => x != null).ToList();
+        while(valids.Count > 0)
+        {
+            int minIndex = 0;
+            for(int i = 1; i < valids.Count; i++)
+            {
+                if(valids[i].val < valids[minIndex].val)
+                {
+                    minIndex = i;
+                }
+            }
+
+            tail.next = valids[minIndex];
+            tail = tail.next;
+            valids[minIndex] = valids[minIndex].next;
+
+            if (valids[minIndex].next == null)
+            {
+                valids.RemoveAt(minIndex);
+            }
+            else
+            {
+                valids[minIndex] = valids[minIndex].next;
+            }
+        }
+        
+        return dummy.next;
+    }
+}
