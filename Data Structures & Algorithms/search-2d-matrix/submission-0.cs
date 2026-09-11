ca87706eabe12fee6006e8492d0522b68fd18935
Bence Holicska <101716648+holicskabence@users.noreply.github.com>
2026-09-11 15:05:06 +0200

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int m = 0;
+        int n = matrix[0].Length - 1;
+        
+        while(m < matrix.Length)
+        {
+            if(target <= matrix[m][n] && target >= matrix[m][0])
+            {
+                int left = 0;
+                int right = n;
+
+                while(left < right)
+                {
+
+                    int middle = (right + left) / 2 + left;
+
+                    if(matrix[m][middle] == target){
+                        return true;
+                    }
+
+                    if(matrix[m][middle] > target)
+                    {
+                        left = middle + 1;
+                    }
+                    else
+                    {
+                        right = middle - 1;
+                    }
+                }
+
+                if(matrix[m][right] == target){
+                    return true;
+                }
+            }
+
+            m++;
+        }
+
+        return false;
+    }
+}
