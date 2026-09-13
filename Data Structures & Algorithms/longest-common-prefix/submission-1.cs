3dd26582a8aa2de5565727183fb184fbee03ee93
Bence Holicska <101716648+holicskabence@users.noreply.github.com>
2026-09-13 11:15:30 +0200

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        var sorted = strs.OrderBy(x => x.Length).ThenBy(x => x).ToList();
+        string prefix = sorted[0];
+        bool isValid = true;
+
+        while(prefix.Length > 0)
+        {
+            for(int i = 1; i < sorted.Count; i++)
+            {
+                if(sorted[i].Substring(0, prefix.Length) != prefix)
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if(isValid)
+            {
+                return prefix;
+            }
+            else
+            {
+                isValid = true;
+            }
+
+            Console.WriteLine(prefix);
+            prefix = sorted[0].Substring(0, prefix.Length - 1);
+        }
+
+        return prefix;
+    }
+}
